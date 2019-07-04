3d210200e8f030c68b8a8624fa176b1649fd1649
shafrazi <shafrazi@live.com>
2019-07-04 23:21:45 +0530

    bubble sort complete

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -23,3 +23,26 @@ def sort(array)
   end
   array
 end
+
+def bubble_sort_by(array)
+  all_sorted = false
+  while !all_sorted
+    all_sorted = true
+    array.each_with_index do |element, i|
+      if array[i + 1] != nil
+        if (yield(element, array[i+1]) > 0)
+          array[i] = array[i+1]
+          array[i+1] = element
+          all_sorted = false
+        end
+      end
+    end
+  end
+  array
+end
+
+new_array = bubble_sort_by(["hi","hello","hey", "I"]) do |left, right|
+  left.length - right.length
+end
+
+p new_array
