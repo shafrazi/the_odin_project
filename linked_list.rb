e61ed2eb24bdc01e55a5b0eaf407e8a22ce62e0e
shafrazi <shafrazi@live.com>
2019-09-02 02:33:26 +0530

    completed up to to_s method

--- linked_list.rb
+++ linked_list.rb
@@ -13,6 +13,7 @@ class LinkedList
     else
       self.tail.next_node = node
       self.tail = node
+      node.next_node = nil
     end
     self.size += 1
   end
@@ -25,6 +26,59 @@ class LinkedList
       self.head = node
     end
     self.size += 1
+  end
+
+  def at(index)
+    i = 0
+    node = self.head
+    while i < index
+      node = node.next_node
+      i += 1
+    end
+    node
+  end
+
+  def pop
+    if self.tail
+      self.tail = self.at(self.size-2) 
+      self.tail.next_node = nil
+      self.size -= 1
+    end
+  end
+
+  def contains?(value)
+    i = 0
+    while i < self.size
+      node = self.at(i)
+      if node.value == value
+        return true
+      end
+      i += 1
+    end
+    return false
+  end
+
+  def find(data)
+    i = 0
+    while i < self.size
+      node = self.at(i)
+      if node.value == data
+        return i
+      end
+      i += 1
+    end
+    return nil
+  end
+
+  def to_s
+    array = []
+    i = 0
+    while i < self.size
+      node = self.at(i)
+      array.push(node.value)
+      i += 1
+    end
+    puts array.join(' -> ')
   end
 end
 
@@ -39,12 +93,24 @@ end
 node1 = Node.new(1)
 node2 = Node.new(2)
 node0 = Node.new(0)
+node3 = Node.new(3)
 
 linked_list = LinkedList.new
 linked_list.append(node1)
 linked_list.append(node2)
 linked_list.prepend(node0)
-p linked_list.size
-p linked_list.head.value
-p linked_list.tail.value
+puts "size of list is #{linked_list.size}"
+puts "value of head: #{linked_list.head.value}"
+puts "value of tail: #{linked_list.tail.value}"
+# p linked_list.tail.next_node
+linked_list.append(node3)
+p linked_list.contains?(2)
+p linked_list.find(2)
+
+puts "size of list is #{linked_list.size}"
+puts "value of head: #{linked_list.head.value}"
+puts "value of tail: #{linked_list.tail.value}"
+
+linked_list.to_s
+
 
